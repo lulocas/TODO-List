abe2c49ab9fa6fca74d147bd441aebcb91ef4cc7
Luisa Deschamps <andradeps434@gmail.com>
2025-02-10 11:53:49 -0300

    Criação da caixa de edit

--- Front/todo.js
+++ Front/todo.js
@@ -4,6 +4,7 @@ alert(adicionar);
 var botoes = document.getElementById('botoes');
 alert(botoes);
 var main = document.getElementsByTagName('main');
+var notas = document.getElementsByClassName('nota');
 
 class Tarefa{
     nome;
@@ -24,6 +25,245 @@ class Tarefa{
 }
 
 var tarefas = [];
+
+function adicionarListenersNotas() {
+    var notas = document.getElementsByClassName('nota');
+    
+    for (var i = 0; i < notas.length; i++) {
+        (function(index) {
+            notas[index].addEventListener("click", function() {
+                var caixa = document.createElement('div');
+                caixa.classList.add("container");
+                tarefasDiv.parentElement.appendChild(caixa);
+    
+                var editcaixa = document.createElement('div');
+                editcaixa.classList.add("editcaixa");
+                caixa.appendChild(editcaixa);
+
+                // Ciação div do botao sair
+                var botaoX = document.createElement('div');
+                botaoX.id = "botaoX";
+                editcaixa.appendChild(botaoX);
+                // Criação botao sair
+                var bSair = document.createElement('button');
+                bSair.id = "sair";
+                bSair.textContent = "X";
+                botaoX.appendChild(bSair);
+
+                 // Criação div labels para ficar os inputs e labels
+                var labels = document.createElement('div');
+                labels.classList.add("labels");
+                editcaixa.appendChild(labels);
+
+                var caixinhaNome = document.createElement('div');
+                caixinhaNome.classList.add("caixinha");
+                labels.appendChild(caixinhaNome);
+                var pNome = document.createElement('p');
+                pNome.classList.add("editnotas");
+                pNome.id = "pNome";
+                
+                caixinhaNome.appendChild(pNome);
+                var inputNome = document.createElement('input');
+                inputNome.classList.add('inputEscondido');
+                inputNome.type = "text";
+                inputNome.placeholder = "Digite...";
+                inputNome.style.display ="none";
+                caixinhaNome.appendChild(inputNome);
+
+                var caixinhaDescricao = document.createElement('div');
+                caixinhaDescricao.classList.add("caixinha");
+                labels.appendChild(caixinhaDescricao);
+                var pDescricao = document.createElement('p');
+                pDescricao.classList.add("editnotas");
+                pDescricao.id = "pDescricao";
+                
+                caixinhaDescricao.appendChild(pDescricao);
+                var inputDescricao = document.createElement('input');
+                inputDescricao.classList.add('inputEscondido');
+                inputDescricao.type = "text";
+                inputDescricao.id = "inputDesIdit";
+                inputDescricao.placeholder = "Digite...";
+                inputDescricao.style.display ="none";
+                caixinhaDescricao.appendChild(inputDescricao);
+
+                var caixinhaCategoria= document.createElement('div');
+                caixinhaCategoria.classList.add("caixinha");
+                labels.appendChild(caixinhaCategoria);
+                var pCategoria = document.createElement('p');
+                pCategoria.classList.add("editnotas");
+                pCategoria.id = "pCategoria";
+                
+                caixinhaCategoria.appendChild(pCategoria);
+                var inputCategoria = document.createElement('input');
+                inputCategoria.classList.add('inputEscondido');
+                inputCategoria.type = "text";
+                inputCategoria.placeholder = "Digite...";
+                inputCategoria.style.display ="none";
+                caixinhaCategoria.appendChild(inputCategoria);
+
+                var caixinhaData = document.createElement('div');
+                caixinhaData.classList.add("caixinha");
+                labels.appendChild(caixinhaData);
+                var pData = document.createElement('p');
+                pData.classList.add("editnotas");
+                pData.id = "pData";
+                
+                caixinhaData.appendChild(pData);
+                var inputData = document.createElement('input');
+                inputData.classList.add('inputEscondido');
+                inputData.type = "date";
+                inputData.style.display ="none";
+                caixinhaData.appendChild(inputData);
+
+
+                var editNotasElements = document.getElementsByClassName('editnotas');
+                var inputEscondidos = document.getElementsByClassName('inputEscondido');
+
+                for (var i = 0; i < editNotasElements.length; i++) {
+                    (function(index) {
+                        editNotasElements[index].addEventListener("click", function() {
+                            inputEscondidos[index].style.display = "block";
+                        });
+                    })(i);
+                }
+
+                // Criação da div de selecionar
+                var selecionar = document.createElement('div');
+                selecionar.classList.add("selecionar");
+                editcaixa.appendChild(selecionar);
+                var tituloPrioridade = document.createElement('p');
+                tituloPrioridade.textContent = "Nível de prioridade: "
+                selecionar.appendChild(tituloPrioridade);
+                // Criação da div de prioridade
+                var nivelPrioridade = document.createElement('div');
+                nivelPrioridade.classList.add("nivelprioridade");
+                selecionar.appendChild(nivelPrioridade);
+                // Criação dos elementos de input e label
+                var inputR1 = document.createElement('input');
+                inputR1.type = "radio";
+                inputR1.id = "nivel1";
+                inputR1.value = "1";
+                inputR1.name = "prioridade";
+                nivelPrioridade.appendChild(inputR1);
+                var labelR1 = document.createElement('label');
+                labelR1.htmlFor = "nivel1";
+                labelR1.textContent = "1";
+                nivelPrioridade.appendChild(labelR1);
+
+                var inputR2 = document.createElement('input');
+                inputR2.type = "radio";
+                inputR2.id = "nivel2";
+                inputR2.value = "2";
+                inputR2.name = "prioridade";
+                nivelPrioridade.appendChild(inputR2);
+                var labelR2 = document.createElement('label');
+                labelR2.htmlFor = "nivel2";
+                labelR2.textContent = "2";
+                nivelPrioridade.appendChild(labelR2);
+
+                var inputR3 = document.createElement('input');
+                inputR3.type = "radio";
+                inputR3.id = "nivel3";
+                inputR3.value = "1";
+                inputR3.name = "prioridade";
+                nivelPrioridade.appendChild(inputR3);
+                var labelR3 = document.createElement('label');
+                labelR3.htmlFor = "nivel3";
+                labelR3.textContent = "3";
+                nivelPrioridade.appendChild(labelR3);
+
+                var inputR4 = document.createElement('input');
+                inputR4.type = "radio";
+                inputR4.id = "nivel4";
+                inputR4.value = "1";
+                inputR4.name = "prioridade";
+                nivelPrioridade.appendChild(inputR4);
+                var labelR4 = document.createElement('label');
+                labelR4.htmlFor = "nivel4";
+                labelR4.textContent = "4";
+                nivelPrioridade.appendChild(labelR4);
+
+                var inputR5 = document.createElement('input');
+                inputR5.type = "radio";
+                inputR5.id = "nivel5";
+                inputR5.value = "1";
+                inputR5.name = "prioridade";
+                nivelPrioridade.appendChild(inputR5);
+                var labelR5 = document.createElement('label');
+                labelR5.htmlFor = "nivel5";
+                labelR5.textContent = "5";
+                nivelPrioridade.appendChild(labelR5);
+
+                // Ciração da div dos botoes
+                var statuselect = document.createElement('div');
+                statuselect.classList.add("statuselect");
+                editcaixa.appendChild(statuselect);
+                // Ciração dos botoes
+                var botaoToDo = document.createElement('button');
+                botaoToDo.classList.add("botaoselect");
+                botaoToDo.id = "selecttodo";
+                botaoToDo.textContent = "To do";
+                statuselect.appendChild(botaoToDo);
+
+                var botaoDoing = document.createElement('button');
+                botaoDoing.classList.add("botaoselect");
+                botaoDoing.id = "selectdoing";
+                botaoDoing.textContent = "Doing";
+                statuselect.appendChild(botaoDoing);
+
+                var botaoDone = document.createElement('button');
+                botaoDone.classList.add("botaoselect");
+                botaoDone.id = "selectdone";
+                botaoDone.textContent = "Done";
+                statuselect.appendChild(botaoDone);
+
+
+                var salvarEx = document.createElement('div');
+                salvarEx.classList.add("salvarEx");
+                editcaixa.appendChild(salvarEx);
+                var botaoSalvar = document.createElement('button');
+                botaoSalvar.classList.add("bsalvarEx");
+                botaoSalvar.textContent = "Salvar";
+                salvarEx.appendChild(botaoSalvar);
+                var botaoExcluir = document.createElement('button');
+                botaoExcluir.classList.add("bsalvarEx");
+                botaoExcluir.textContent = "Excluir";
+                salvarEx.appendChild(botaoExcluir);
+
+                bSair.onclick = function(){
+                    editcaixa = document.querySelector('.caixa');
+                    if (editcaixa) {
+                        editcaixa.remove();
+                    }
+                    caixa = document.querySelector('.container');
+                    if (caixa) {
+                        caixa.remove();
+                    }
+                }
+
+                function preencherCampos(nome) {
+                    for (var i = 0; i < tarefas.length; i++) {
+                        if (tarefas[i].nome === nome) {
+                            pNome.textContent = tarefas[i].nome;
+                            inputNome.value = tarefas[i].nome;
+                            pDescricao.textContent = tarefas[i].descricao;
+                            inputDescricao.value = tarefas[i].descricao;
+                            pCategoria.textContent = tarefas[i].categoria;
+                            inputCategoria.value = tarefas[i].categoria;
+                            pData.textContent = tarefas[i].data;
+                            inputData.value = tarefas[i].data;
+                        }
+                    }
+                }
+
+                preencherCampos();
+
+            });
+        })(i);
+    }
+}
+
+adicionarListenersNotas();
 
 adicionar.onclick= function(){
 
@@ -131,7 +371,7 @@ adicionar.onclick= function(){
 
     var inputR3 = document.createElement('input');
     inputR3.type = "radio";
-    inputR3.id = "nivel1";
+    inputR3.id = "nivel3";
     inputR3.value = "1";
     inputR3.name = "prioridade";
     nivelPrioridade.appendChild(inputR3);
@@ -142,7 +382,7 @@ adicionar.onclick= function(){
 
     var inputR4 = document.createElement('input');
     inputR4.type = "radio";
-    inputR4.id = "nivel1";
+    inputR4.id = "nivel4";
     inputR4.value = "1";
     inputR4.name = "prioridade";
     nivelPrioridade.appendChild(inputR4);
@@ -153,7 +393,7 @@ adicionar.onclick= function(){
 
     var inputR5 = document.createElement('input');
     inputR5.type = "radio";
-    inputR5.id = "nivel1";
+    inputR5.id = "nivel5";
     inputR5.value = "1";
     inputR5.name = "prioridade";
     nivelPrioridade.appendChild(inputR5);
@@ -264,6 +504,17 @@ adicionar.onclick= function(){
 
     }
 
+    bSair.onclick = function(){
+        caixa2 = document.querySelector('.caixa');
+        if (caixa2) {
+            caixa2.remove();
+        }
+        caixa = document.querySelector('.container');
+        if (caixa) {
+            caixa.remove();
+        }
+    }
+
     botaoToDo.onclick = function(){
         caixa2.style.backgroundColor = "tomato";
     }
@@ -276,3 +527,12 @@ adicionar.onclick= function(){
 
 }
 
+var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+        if (mutation.addedNodes.length > 0) {
+            adicionarListenersNotas();
+        }
+    });
+});
+
+observer.observe(tarefasDiv, { childList: true });
